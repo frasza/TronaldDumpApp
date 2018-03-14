9dcd5d1fbdc8411073fe47640ab284d5c4c0bf0b
Zan Fras <zan.fraas@gmail.com>
2018-03-14 10:57:31 +0100

    Add pods and basic layout

---
+++ TronaldDumpApp/TronaldDumpViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TronaldDumpViewController.swift
+//  TronaldDumpApp
+//
+//  Created by Žan Fras on 14/03/2018.
+//  Copyright © 2018 Žan Fras. All rights reserved.
+//
+
+import UIKit
+
+class TronaldDumpViewController: UIViewController {
+
+    @IBOutlet weak var tronaldDumpImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK: - Fetch New Button
+    /***************************************************************/
+    
+    @IBAction func fetchNewQuote(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+
+
+}
+
