38a6792295bc4c090553b0af1df1c7355d024da3
Zan Fras <zan.fraas@gmail.com>
2018-03-14 11:13:05 +0100

    Fully functional

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+
+target 'TronaldDumpApp' do
+
+  use_frameworks!
+
+  # Pods for TronaldDumpApp
+pod 'Alamofire'
+pod 'SwiftyJSON'
+pod 'SVProgressHUD'
+
+end
--- TronaldDumpApp/TronaldDumpViewController.swift
+++ TronaldDumpApp/TronaldDumpViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class TronaldDumpViewController: UIViewController {
+    
+    let tronaldDumpHTTP = "https://api.tronalddump.io/random/quote"
 
     @IBOutlet weak var tronaldDumpImage: UIImageView!
+    @IBOutlet weak var quoteLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getRandomQuote()
     }
     
     //MARK: - Fetch New Button
@@ -22,11 +28,45 @@ class TronaldDumpViewController: UIViewController {
     
     @IBAction func fetchNewQuote(_ sender: UIButton) {
         
-        
+        getRandomQuote()
         
     }
     
+    //MARK: - Networking
+    /***************************************************************/
+    func getRandomQuote() {
+        
+        Alamofire.request(tronaldDumpHTTP, method: .get, parameters: nil).responseJSON { (response) in
+            if response.result.isSuccess {
+                let quoteJSON = JSON(response.result.value!)
+                
+                self.getQuoteFromJSON(json: quoteJSON)
+            } else {
+                print("Error fetching quote, \(response.result.error!)")
+            }
+        }
+        
+    }
+    
+    //MARK: - JSON
+    /***************************************************************/
+    
+    func getQuoteFromJSON(json: JSON) {
+        
+        let quote = json["value"].stringValue
+        
+        updateQuoteLabel(with: quote)
+        
+    }
+    
+    //MARK: - Update UI
+    /***************************************************************/
 
+    func updateQuoteLabel(with quote: String) {
+        
+        quoteLabel.text = quote
+        
+    }
 
 }
 
